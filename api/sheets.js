0e904858b30ce365a950c45bbb4c0fd038cd2ab9
IAmPengu <0xSolaris+pengu@proton.me>
2024-03-25 18:34:37 -0700

    Checking for wallet address

--- api/sheets.js
+++ api/sheets.js
@@ -1,10 +1,15 @@
 import { google } from 'googleapis'
 import moment from 'moment'
+import { isAddress } from 'web3-validator'
 
 export async function GET(request) {
 
   const address = request.url.split("=")[1]
   const now = moment().format('LLL')
+
+  if (!isAddress(address)) {
+    return new Response('Invalid address')
+  }
 
   const client = new google.auth.JWT(process.env.VITE_CLIENT_EMAIL, null, process.env.VITE_PRIVATE_KEY, ['https://www.googleapis.com/auth/spreadsheets'])
   const sheet = google.sheets({ version: 'v4', auth: client})
